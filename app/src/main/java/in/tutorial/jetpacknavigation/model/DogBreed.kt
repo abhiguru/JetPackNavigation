44a6340984f4398f28d0aa3f858294147e4c0d3a
abhiguru <abhinavguru@gmail.com>
2023-02-06 01:37:39 +0530

    MVVM with Retrofit2"GET" and RxJava"Single"

--- app/src/main/java/in/tutorial/jetpacknavigation/model/DogBreed.kt
+++ app/src/main/java/in/tutorial/jetpacknavigation/model/DogBreed.kt
@@ -1,11 +1,21 @@
 package `in`.tutorial.jetpacknavigation.model
 
+import com.google.gson.annotations.SerializedName
+
+//https://raw.githubusercontent.com/DevTides/DogsApi/master/dogs.json
 data class DogBreed (
+    @SerializedName("id")
     val breedId: String?,
+    @SerializedName("name")
     val dogBreed: String?,
+    @SerializedName("life_span")
     val lifespan: String?,
+    @SerializedName("breed_group")
     val breedGroup:String?,
+    @SerializedName("bred_for")
     val bredFor:String?,
+    @SerializedName("temperament")
     val temperament: String?,
+    @SerializedName("url")
     val imageUrl:String?
 )
--- app/src/main/java/in/tutorial/jetpacknavigation/viewmodel/ListViewModel.kt
+++ app/src/main/java/in/tutorial/jetpacknavigation/viewmodel/ListViewModel.kt
@@ -3,22 +3,48 @@ package `in`.tutorial.jetpacknavigation.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import `in`.tutorial.jetpacknavigation.model.DogBreed
+import `in`.tutorial.jetpacknavigation.model.DogsApiService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class ListViewModel:ViewModel() {
+    private val dogsService = DogsApiService()
+    private val disposable = CompositeDisposable()
+
     val dogs = MutableLiveData<List<DogBreed>>()
     val dogsLoadError = MutableLiveData<Boolean>()
     val loading = MutableLiveData<Boolean>()
 
     fun refresh(){
-        val dog1 = DogBreed("1", "Corgi","15 years",
-            "breedGroup", "bredFor","temperament", "")
-        val dog2 = DogBreed("2", "Labrador","1 years",
-            "breedGroup", "bredFor","temperament", "")
-        val dog3 = DogBreed("1", "Rotwiller","20 years",
-            "breedGroup", "bredFor","temperament", "")
-        val dogList = arrayListOf<DogBreed>(dog1, dog2, dog3)
-        dogs.value = dogList
-        dogsLoadError.value = false
-        loading.value = false
+        fetchFromRemote()
+    }
+    private fun fetchFromRemote(){
+        loading.value = true
+        disposable.add(
+            dogsService.getDogs()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<List<DogBreed>>(){
+                    override fun onSuccess(dogsList: List<DogBreed>) {
+                        dogs.value = dogsList
+                        dogsLoadError.value = false
+                        loading.value = false
+                    }
+
+                    override fun onError(e: Throwable) {
+                        dogsLoadError.value = true
+                        loading.value = false
+                        e.printStackTrace()
+                    }
+
+                })
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
     }
 }
